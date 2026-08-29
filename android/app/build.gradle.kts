e242f47d13a3d9eedb37f4d0937163eaca5eda8c
Miguel Fuertes <mjfuertesf@gmail.com>
2026-08-29 15:52:26 +0200

    chore: bump version to 0.3.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,9 +19,9 @@ android {
             abiFilters.add("armeabi-v7a")
         }
         versionCode = if (project.ext.has("versionCode"))
-            project.ext.get("versionCode").toString().toInt() else 34
+            project.ext.get("versionCode").toString().toInt() else 35
         versionName = if (project.ext.has("versionName"))
-            project.ext.get("versionName").toString() else "0.3.4"
+            project.ext.get("versionName").toString() else "0.3.5"
         base.archivesName = "Ava-$versionName"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
